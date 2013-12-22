bb205f2c39d40a5eb77c268aa01c501ef0864a5c
David Stanley <davidstanley01@gmail.com>
2013-12-21 23:59:32 -0600

    formatted with doc blocks

--- src/MovieCommit/MovieCommit.php
+++ src/MovieCommit/MovieCommit.php
@@ -61,18 +61,22 @@ class MovieCommit
         return $this->getLine($movie);
     }
 
+    /**
+     * Get Quote by ID
+     *
+     * @param  string $id the encoded json array
+     * @return array      The quote we are looking for
+     */
     public function getQuoteById($id)
     {
         // decode the id
         $movie = json_decode(base64_decode($id), true);
 
         if ($movie) {
-            list($movieName, $title, $line) = $movie;
-            return [
-                'line' => $line,
-                'title' => $title,
-                'permalink' => $id
-            ];
+
+            list($movieName, $line) = $movie;
+            return $this->getLineByNumber($movieName, $line);
+
         } else {
             return $this->default;
         }
@@ -98,38 +102,76 @@ class MovieCommit
      * @param  string $movie The movie we are looking for
      * @return array         An associative array with out datas
      */
-    protected function getLine($movie)
+    protected function getLine($movieName)
     {
-        // Get the file and read it into an array
-        $filename = $this->path . "$movie.txt";
-
-        // supressing the errors is stupid, but I'm tired
-        $lines = @file($filename, FILE_IGNORE_NEW_LINES);
+        // get the array of lines
+        $lines = $this->loadData($movieName);
 
         // If we have lines...
         if ($lines) {
-
-            // The first line of the file is the movie title.  Grab it and get rid of it.
-            $title = $lines[0];
-            unset($lines[0]);
-            $lines = array_values($lines);
-
-            // get our line.  subtract 1 to account for zero index
-            $line = (count($lines) == 1) ? 0 : rand(1, count($lines)) - 1;
-
-            // return the obvious
-            return array(
-                "line"  => $lines[$line],
-                "title" => $title,
-                "permalink" => base64_encode(
-                    json_encode([$movie, $title, $lines[$line]])
-                )
-            );
-
+            $lineNumber = (count($lines) == 1) ? 0 : rand(1, count($lines)) - 1;
+            return $this->formatResponseArray($lines, $lineNumber, $movieName);
         } else {
-
             // something.  anything
             return $this->default;
         }
+    }
+
+    /**
+     * Get line from file by line number in file
+     *
+     * @param  string $movieName  the filename
+     * @param  int    $lineNumber The line number starting at 1
+     * @return array              The response data
+     */
+    protected function getLineByNumber($movieName, $lineNumber)
+    {
+        // get the array of lines
+        $lines = $this->loadData($movieName);
+
+        // If we have lines...
+        if ($lines) {
+            return $this->formatResponseArray($lines, $lineNumber, $movieName);
+        } else {
+            return $this->default;
+        }
+    }
+
+    /**
+     * Load the file and return the data
+     *
+     * @param  string $movieName the filename
+     * @return array             the raw array of all the lines
+     */
+    protected function loadData($movieName)
+    {
+        // Get the file and read it into an array
+        $filename = $this->path . "$movieName.txt";
+
+        // supressing the errors is stupid, but I'm tired
+        return @file($filename, FILE_IGNORE_NEW_LINES);
+    }
+
+    /**
+     * Format data for response
+     *
+     * @param  array  $lines      The array of lines
+     * @param  int    $lineNumber The index of the line we want
+     * @param  string $movieName  The name of the file
+     * @return array              The formatted data
+     */
+    protected function formatResponseArray($lines, $lineNumber, $movieName)
+    {
+        // The first line of the file is the movie title.  Grab it and get rid of it.
+        $title = $lines[0];
+        unset($lines[0]);
+        $lines = array_values($lines);
+
+        // return the obvious
+        return array(
+            "line"  => $lines[$lineNumber],
+            "title" => $title,
+            "permalink" => base64_encode(json_encode([$movieName, $lineNumber]))
+        );
     }
 }
