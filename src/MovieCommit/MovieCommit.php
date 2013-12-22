0f4863d664947a4a37d31458c807866830604f74
David Stanley <davidstanley01@gmail.com>
2013-12-21 23:41:02 -0600

    added permalinks to clean route

--- src/MovieCommit/MovieCommit.php
+++ src/MovieCommit/MovieCommit.php
@@ -27,6 +27,17 @@ class MovieCommit
     protected $movieCount;
 
     /**
+     * Our default response if something went wrong
+     *
+     * @var array
+     */
+    protected $default = [
+        "line"  => "Dave's not here, man.",
+        "title" => "The Big Lebowski",
+        "permalink" => null
+    ];
+
+    /**
      * Class Constructor
      * @param array $movies The array of movies we have from which to choose
      */
@@ -34,94 +45,91 @@ class MovieCommit
     {
         $this->movies = $movies;
         $this->path = __DIR__ .'/data/';    // set the file path - feels durty
-        $this->line = $this->getLine();     // make the line available
+        $this->line = $this->getQuote();     // make the line available
     }
 
     /**
-     * Get the line
-     * @return string The movie line
+     * Get a Movie Quote
+     *
+     * @param  string $id The id of the quote to retrieve
+     * @return string     The movie line
      */
-    protected function getLine()
+    public function getQuote($id = null)
+    {
+        // with no id, do the random thing...
+        $movie = $this->getMovie();
+        return $this->getLine($movie);
+    }
+
+    public function getQuoteById($id)
+    {
+        // decode the id
+        $movie = json_decode(base64_decode($id), true);
+
+        if ($movie) {
+            list($movieName, $title, $line) = $movie;
+            return [
+                'line' => $line,
+                'title' => $title,
+                'permalink' => $id
+            ];
+        } else {
+            return $this->default;
+        }
+    }
+
+    /**
+     * Get a Movie from the list
+     *
+     * @return string Path to a movie file
+     */
+    protected function getMovie()
     {
         // Get the movie we want. Subtract 1 to account for zero index
         $movie = (count($this->movies) == 1) ? 0 : rand(1, count($this->movies)) - 1;
 
         // What is our movie?
-        $movieName = $this->movies[$movie];
+        return $this->movies[$movie];
+    }
 
+    /**
+     * Get a line from a specific movie
+     *
+     * @param  string $movie The movie we are looking for
+     * @return array         An associative array with out datas
+     */
+    protected function getLine($movie)
+    {
         // Get the file and read it into an array
-        $filename = $this->path . "$movieName.txt";
+        $filename = $this->path . "$movie.txt";
 
         // supressing the errors is stupid, but I'm tired
         $lines = @file($filename, FILE_IGNORE_NEW_LINES);
 
-        // The first line of the file is the movie title.  Grab it and get rid of it.
-        $title = $lines[0];
-        unset($lines[0]);
-        $lines = array_values($lines);
-
+        // If we have lines...
         if ($lines) {
-            // get the line we want. Subtract 1 to account for zero index
+
+            // The first line of the file is the movie title.  Grab it and get rid of it.
+            $title = $lines[0];
+            unset($lines[0]);
+            $lines = array_values($lines);
+
+            // get our line.  subtract 1 to account for zero index
             $line = (count($lines) == 1) ? 0 : rand(1, count($lines)) - 1;
 
             // return the obvious
             return array(
                 "line"  => $lines[$line],
                 "title" => $title,
-                "permalink" => base64_encode(json_encode(array($movie, $line)))
+                "permalink" => base64_encode(
+                    json_encode([$movie, $title, $lines[$line]])
+                )
             );
 
         } else {
 
             // something.  anything
-            return array(
-                "line"  => "Dave's not here, man.",
-                "title" => "The Big Lebowski",
-                "permalink" => null
-            );
-        }
-    }
-
-    /**
-     * Get a specific line
-     * @param  string $id The id of the quote to retrieve
-     * @return string The movie line
-     */
-    public function getSpecificLine($id)
-    {
-        list($movie, $line) = json_decode(base64_decode($id), true);
-
-        // What is our movie?
-        $movieName = $this->movies[$movie];
-
-        // Get the file and read it into an array
-        $filename = $this->path . "$movieName.txt";
-
-        // supressing the errors is stupid, but I'm tired
-        $lines = @file($filename, FILE_IGNORE_NEW_LINES);
-
-        // The first line of the file is the movie title.  Grab it and get rid of it.
-        $title = $lines[0];
-        unset($lines[0]);
-        $lines = array_values($lines);
-
-        if ($lines) {
-
-            // return the obvious
-            return array(
-                "line"  => $lines[$line],
-                "title" => $title,
-                "permalink" => base64_encode(json_encode(array($movie, $line)))
-            );
-
-        } else {
-
-            // something.  anything
-            return array(
-                "line"  => "Dave's not here, man.",
-                "title" => "The Big Lebowski",
-                "permalink" => null
-            );
+            return $this->default;
         }
     }
 }
